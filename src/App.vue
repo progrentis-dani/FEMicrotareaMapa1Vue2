88844cea14348ea35c6faaa9b690e8bf3bcaa22d
progrentis-dani <soporte.dc502@gmail.com>
2025-05-13 12:03:07 -0600

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@
     <!-- Menú de selección -->
     <div v-if="!varianteSeleccionada" class="menu-centro">
       <h2>Selecciona una variante</h2>
-      <button @click="iniciarVariante(1)">Variante 1</button>
-      <button @click="iniciarVariante(2)">Variante 2</button>
+      <button v-on:click="iniciarVariante(1)">Variante 1</button>
+      <button v-on:click="iniciarVariante(2)">Variante 2</button>
     </div>
 
-    <!-- Solo se muestra el juego si selecciona la variante 1 -->
+    <!-- Solo se muestra el juego si selecciona la variante 1 o 2 -->
     <JuegoMapa1 v-if="varianteSeleccionada === 1" />
     <JuegoMapa2 v-if="varianteSeleccionada === 2" />
   </div>
@@ -20,29 +20,32 @@ import JuegoMapa2 from './components/JuegoMapa2.vue'
 export default {
   name: 'App',
   components: {
-    JuegoMapa1,
-    JuegoMapa2
+    JuegoMapa1: JuegoMapa1,
+    JuegoMapa2: JuegoMapa2
   },
-  data() {
+  data: function () {
     return {
       varianteSeleccionada: null
-    }
+    };
   },
   methods: {
-    iniciarVariante(variante) {
-      this.varianteSeleccionada = variante
+    iniciarVariante: function (variante) {
+      this.varianteSeleccionada = variante;
     }
   }
-}
+};
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
+/* Asegúrate de que la fuente externa no cause problemas en iOS 9 */
+@import url('https://fonts.googleapis.com/css?family=Press+Start+2P');
 
 .menu-centro {
   position: absolute;
   top: 50%;
   left: 50%;
+  /* El transform puede fallar en navegadores antiguos si no tiene prefijo */
+  -webkit-transform: translate(-50%, -50%);
   transform: translate(-50%, -50%);
   background-color: #beb9b9;
   padding: 1.5rem 2rem;
@@ -63,6 +66,7 @@ export default {
   font-family: 'Press Start 2P', cursive;
   cursor: pointer;
   text-shadow: 1px 1px #000;
+  -webkit-transition: -webkit-transform 0.1s, background-color 0.2s;
   transition: transform 0.1s, background-color 0.2s;
 }
 </style>
