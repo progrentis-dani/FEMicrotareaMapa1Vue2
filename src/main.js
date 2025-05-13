18643fa849b923939d6f6bb5a4756f672eaa9bac
progrentis-dani <soporte.dc502@gmail.com>
2025-05-13 15:49:36 -0600

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+import 'core-js/stable' // ← polyfills ES6/ES7 para navegadores antiguos como iOS 9
+import 'regenerator-runtime/runtime' // ← soporte para async/await
+
 import Vue from 'vue'
 import App from './App.vue'
 
